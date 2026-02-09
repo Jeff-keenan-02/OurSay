ba118ff4f50f0b08812577c00a08fd4855924047
Jeff-keenan-02 <2t4kc9ksv5@privaterelay.appleid.com>
2026-02-09 16:46:23 +0000

    Docker files

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,11 +1,22 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'oursay',
+//   password: 'RILEY2015',
+//   database: 'oursaydb',
+//   port: 5431,
+// });
+
+// module.exports = pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'oursay',
-  password: 'RILEY2015',
-  database: 'oursaydb',
-  port: 5431,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5431,
+  user: process.env.DB_USER || "oursay",
+  password: process.env.DB_PASSWORD || "RILEY2015",
+  database: process.env.DB_NAME || "oursaydb",
 });
 
 module.exports = pool;
