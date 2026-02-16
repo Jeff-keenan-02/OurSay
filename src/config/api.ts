98d59e133e8addf53ce7ff4da4aa92ba12511cf8
Jeff-keenan-02 <2t4kc9ksv5@privaterelay.appleid.com>
2026-02-16 13:03:05 +0000

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -10,4 +10,4 @@
 {/* Render database */}
   export const API_BASE_URL = __DEV__
   ? 'http://192.168.0.18:3000'
-  : 'https://your-render-service-name.onrender.com';
+  : 'https://oursay.onrender.com/';
